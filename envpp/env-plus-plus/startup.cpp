f099c777eb0929e84f5272fa8922e05f81cd9f75
saparkhid <parkhidpage@gmail.com>
2025-09-18 16:35:07 +0330

    adds post-build event to copy tcc compiler near the exe

--- envpp/env-plus-plus/startup.cpp
+++ envpp/env-plus-plus/startup.cpp
@@ -1,7 +1,21 @@
+#include <windows.h>
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
+std::string GetExePath() {
+    char buffer[MAX_PATH];
+    GetModuleFileNameA(NULL, buffer, MAX_PATH);
+    std::string::size_type pos = std::string(buffer).find_last_of("\\/");
+    return std::string(buffer).substr(0, pos);
+}
+
 int main() {
 
-	cout << "Hi Sap";
+    string outputSourceFile = "sample.c";
+    string outputExeFile = "sample.exe";
+    string tccCompileCommand = GetExePath() + "\\tcc\\tcc.exe "+ GetExePath() + "\\"+ outputSourceFile+ " -o " + GetExePath() + "\\"+ outputExeFile;
+	system(tccCompileCommand.c_str());
 	return 0;
 }
+
